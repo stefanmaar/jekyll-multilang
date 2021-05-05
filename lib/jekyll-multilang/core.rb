354153608fd289e637867acafbfc4a3418b97c3c
Stefan Mertl <info@mertl-research.at>
2021-05-05 19:45:39 +0200

    Added the default configuration.

--- lib/jekyll-multilang/core.rb
+++ lib/jekyll-multilang/core.rb
@@ -12,11 +12,25 @@ module JekyllMultilang
         attr_accessor :default_lang
         
         # The translation data read from the yaml files.
-        attr_accessor :parsed_translation;
+        attr_accessor :parsed_translation
+
+        # The default configuration.
+        attr_accessor :defaults
       end
 
       self.config = Hash.new
       self.parsed_translation = Hash.new
+      
+      self.defaults = {
+        # The languages of the site (e.g. ['de', 'en']).
+        'languages' => [],
+
+        # The default language (e.g. 'de').
+        # If no default language is given, the first language in languages
+        # is used.
+        'default_language' => nil,
+      }
+      
     end
   end
 end
